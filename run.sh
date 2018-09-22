0d37f327868a24cf0f791fc1f98c436db2778ca8
root <root@ns343404.ip-91-121-141.eu>
2018-09-22 16:33:26 +0000

    update run, add space

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d -p 80:8787 -v /home/folder:/home/rstudio --name "knut-rstudio" --restart always\
+docker run -d -p 80:8787 -v /home/folder:/home/rstudio --name "knut-rstudio" --restart always \
        -e PASSWORD=yourpasswordhere \
        -e VIRTUAL_HOST=rstudio.mydomain.fr \
        -e LETSENCRYPT_HOST=rstudio.mydomain.fr \
