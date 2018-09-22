b2e74e453d19aff48cac59d6352d17a24012739b
root <root@ns343404.ip-91-121-141.eu>
2018-09-22 16:15:35 +0000

    add base dockerfile

---
+++ run.sh
@@ -0,0 +1,6 @@
+docker run -d -p 80:8787 -v /home/folder:/home/rstudio --name "knut-rstudio" --restart always\
+       -e PASSWORD=yourpasswordhere \
+       -e VIRTUAL_HOST=rstudio.mydomain.fr \
+       -e LETSENCRYPT_HOST=rstudio.mydomain.fr \
+       -e LETSENCRYPT_EMAIL=your@gmail.com \
+       rocker/rstudio
---
+++ stop.sh
@@ -0,0 +1,2 @@
+docker stop knut-rstudio
+docker rm knut-rstudio
